eab899ba39d21037555c2ae7ab9c6821b5905561
Moritz Balters <moritz.balters@student.jade-hs.de>
2016-10-27 20:29:48 +0200

    Add Parula es color scheme, add new drawing method and scaling of the spectrogramm to the size of the canvas

--- Spectrogram.js
+++ Spectrogram.js
@@ -1,8 +1,15 @@
 function drawSpec(){
 
-  var cWidth=2060;
+  var cWidth=10000;
   var cHigh=1025;
+  var specLevelHigh=-0;
+  var specLevelLow=-70;
+  var specLevelWidth=Math.abs(specLevelHigh-specLevelLow);
+  var noOfColorSteps=100;
 
+
+creatParula()
+console.log(parulaScale)
 var specData=Audiodata.spectrogram;
 console.log(specData)
 draw()
@@ -11,28 +18,63 @@ draw()
       var canvas = document.getElementById("canvasSpec");
       var ctx = canvas.getContext("2d");
 
+      var tempCanvas = document.createElement("canvas"),
+           tempCtx = tempCanvas.getContext("2d");
+       tempCanvas.width=specData.length;
+       tempCanvas.height=cWidth,specData[1].length;
+
       ctx.clearRect(0,0,cWidth,cHigh);
-        ctx.scale(specData.length/cWidth,specData[1].length/cHigh)
+        //ctx.scale(specData.length/cWidth,specData[1].length/cHigh)
+        console.log(specData.length/cWidth,specData[1].length/cHigh)
+        var pictureData=ctx.createImageData(specData.length,specData[1].length)
+        console.log(pictureData)
+        var nPictureData=0;
 
-         for (var i=1;i<specData.length;i++){
+        for (var j=specData[1].length-1;j>0;j--){
 
-             for (var j=1;j<specData[1].length;j++){
+             for (var i=0;i<specData.length;i++){
+        point=20*Math.log10(specData[i][j]/2048)
+      //  console.log(point)
+        point +=Math.abs(specLevelLow)
+        point=Math.max(point,0)
+        point=Math.min(point,specLevelWidth)
+      //  console.log(point)
+        point /=Math.abs(specLevelLow)
+        point*=noOfColorSteps
+        point=Math.round(point);
+      //  console.log(point)
+
+        for (var kk=0;kk<3;kk++){
+          pictureData.data[nPictureData]=Math.round(parulaScale[kk][point]);
+          nPictureData++
+        }
+        pictureData.data[nPictureData]=255;
+        nPictureData++
+         }
+}
+    /*    for (var j=specData[1].length-1;j>0;j--){
+
+             for (var i=0;i<specData.length;i++){
                point=20*Math.log10(specData[i][j]/2048)
                point +=80;
                point=Math.max(point,0);
                point/=80;
                point*=255;
                point=Math.floor(point);
-
-                ctx.fillStyle = 'rgb(' + point +',' + point  + ',' +
-                                point  + ')';
-                ctx.fillRect(cWidth - i, specData[1].length - j, 1, 1);
-
+               for (var kk=0;kk<3;kk++){
+                 pictureData.data[nPictureData]=point;
+                 nPictureData++
+               }
+               pictureData.data[nPictureData]=255;
+               nPictureData++
                 }
 
-            }
+            }*/
+  console.log(pictureData.data)
 
-
+tempCtx.putImageData(pictureData,0,0)
+ctx.scale(cWidth/specData.length,cHigh/specData[1].length)
+ctx.drawImage(tempCanvas,0,0)
 
 function writeMessage(canvas, message) {
         var context = canvas.getContext('2d');
