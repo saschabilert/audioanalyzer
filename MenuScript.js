c33545e4b1e5bf1f62b444469fb175ecc2a4b8da
saschabilert <sascha.bilert@student.jade-hs.de>
2016-11-16 10:44:51 +0100

    add different overlaps

--- MenuScript.js
+++ MenuScript.js
@@ -17,33 +17,79 @@ function overlap() {
 function chooseDisplay() {
     Audiodata.display = document.getElementById("display").value;
     audioProcessing();
+}
 
+function colormap(){
+    TypeColorScale = +(document.getElementById("colormap").value);
+    changeColorScale();
 }
 
 var startOffset = 0;
 var startTime = 0;
 var audPlay;
+var isPlaying = false;
+var gainNode;
+var playButton = document.getElementById("player");
+playButton.disabled = true;
+playButton.addEventListener("click", toggleSound);
 
-function playSound() {
-    startTime = audioCtx.currentTime;
-    audPlay = audioCtx.createBufferSource();
-    audPlay.buffer = myArrayBuffer;
-    audPlay.loop = false;
-    audPlay.connect(audioCtx.destination);
-    audPlay.start(0, startOffset);
+function toggleSound() {
+    if (!isPlaying) {
+        startTime = audioCtx.currentTime;
+        audPlay = audioCtx.createBufferSource();
+        audPlay.buffer = myArrayBuffer;
+        audPlay.start(0, startOffset);
+        playButton.innerHTML = "Click to pause sound";
+        isPlaying = true;
+
+        window.requestAnimationFrame(drawLinePlay)
+        window.requestAnimationFrame(drawLinePlayWave)
+        //if(audPlay.addEventListener('ended')){
+         //   toggleSound()
+
+
+        gainNode = audioCtx.createGain();
+        audPlay.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+        gainNode.gain.value = 0.5;
+
+
+    } else {
+
+        audPlay.stop();
+        isPlaying = false;
+        playButton.innerHTML = "Click to play sound";
+
+        startOffset += audioCtx.currentTime - startTime;
+
+
+
+
+    }
+
 }
 
-function pauseSound() {
-    audPlay.stop();
-    startOffset += audioCtx.currentTime - startTime;
+
+document.getElementById('volume').addEventListener('input', function() {
+    gainNode.gain.value = this.value;
+
+});
+
+
+function enableButton() {
+   playButton.disabled = !playButton.disabled;
 }
 
-function gainChange() {
-    var gainNode = audioCtx.createGain();
-    audPlay.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-}
+var inputs = document.querySelectorAll( '.audioInput' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+    var label	 = input.nextElementSibling,
+        labelVal = label.innerHTML;
 
-function enableButton(){
-  playButton.disabled = !playButton.disabled;
-}
+    input.addEventListener( 'change', function( e )
+    {
+
+          var fileName = e.target.value.split( '\\' ).pop();
+            label.innerHTML = fileName;
+    });
+});
