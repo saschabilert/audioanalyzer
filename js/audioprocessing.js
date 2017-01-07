36d3d10c3d8d39fc374061fb247c4d244e048847
saschabilert <sascha.bilert@student.jade-hs.de>
2017-01-07 17:15:40 +0100

    added developer as contacts

--- js/audioprocessing.js
+++ js/audioprocessing.js
@@ -17,7 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * contact: moritz.balters@student.jade-hs.de
  * contact: sascha.bilert@student.jade-hs.de
+ * contact: vlad.paul@student.jade-hs.de
  */
 
 // check if AudioContext is supported by the current browser
